f7bed8dd977ebdca56850c4c9cb9ccf2723ea677
Irina Turenko <turenko7irina@mail.ru>
2021-11-01 09:46:26 +0300

    css upd

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-
 import { FcLikePlaceholder } from 'react-icons/fc'
 
 export const Card = ({ title, image, fullRecipe, id }) => {
+  const [active, setActive] = useState(false)
   const liked = useSelector((state) => state.liked)
-const dispatch = useDispatch()
+  const dispatch = useDispatch()
   return (
     <div className='card'>
       <div className='card-header'>
         <h2>{title}</h2>
-        <FcLikePlaceholder onClick={() => dispatch({ type: 'LIKED', payload: Object.values(id)  })}/>
+        <div className='like' onClick={() => {dispatch({ type: 'LIKED', payload: id })}}><FcLikePlaceholder onClick={(e) => {
+          e.target.style.backgrounColor = 'blue'
+          console.log(e.target.style.backgrounColor) }}/></div>
       </div>
 
       <img src={image} alt={title} onClick={() => console.log(liked)} />
